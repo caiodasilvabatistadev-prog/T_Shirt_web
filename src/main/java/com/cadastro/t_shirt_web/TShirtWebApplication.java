78e72d151d039a43943b341bf8e7d67178484cbf
Caio <canalcoisativo@gmail.com>
2026-06-11 17:46:50 -0300

    Create the diretory structury

---
+++ src/main/java/com/cadastro/t_shirt_web/TShirtWebApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.t_shirt_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TShirtWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TShirtWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cadastro/t_shirt_web/TShirtWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro.t_shirt_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TShirtWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
