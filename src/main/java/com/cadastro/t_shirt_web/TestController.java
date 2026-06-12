062237324f2e6983779d7d4c8c5ea6fe1218ce93
Caio <canalcoisativo@gmail.com>
2026-06-12 20:07:44 -0300

    Api is Working!

---
+++ src/main/java/com/cadastro/t_shirt_web/TestController.java
@@ -0,0 +1,13 @@
+package com.cadastro.t_shirt_web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "API is working!";
+    }
+}
