1fe5fdcb57873b344bcdef36a869adfe3cd5697a
Caio <canalcoisativo@gmail.com>
2026-06-16 18:07:09 -0300

    Create Product Entity

--- src/main/java/T_shirt_Entity/Tshirt_Product.java
+++ src/main/java/T_shirt_Entity/Tshirt_Product.java
@@ -1,4 +1,68 @@
 package T_shirt_Entity;
+import jakarta.persistence.*;
 
+@Entity
 public class Tshirt_Product {
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+
+private String name;
+private String description;
+private Double price;
+private Integer stock;
+public Tshirt_Product(){
+
+}
+    public Tshirt_Product(Long id,
+                          String name,
+                          String description,
+                          Double price,
+                          Integer stock) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.stock = stock;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
 }
